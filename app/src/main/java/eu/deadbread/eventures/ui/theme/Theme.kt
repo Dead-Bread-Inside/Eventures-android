456c5e64ae5a1bb4a0b28edc52c61eba895d2960
Khvostov Maksim <mkliverout@gmail.com>
2022-08-31 15:15:03 +0300

    feat: implement colors for light theme

--- app/src/main/java/eu/deadbread/eventures/ui/theme/Theme.kt
+++ app/src/main/java/eu/deadbread/eventures/ui/theme/Theme.kt
@@ -22,9 +22,15 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color.Blue,
+    onPrimary = Color.White,
+    primaryContainer = Color.Blue,
+
+    secondary = Color.White,
+    onSecondary = Color.Black,
+
+    background = Color.White,
+    onBackground = Color.Black
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
